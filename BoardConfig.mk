86fcc9d7fa13e31d7d397bb190b20a4fa68a7925
jenkins <jenkins@teamw.in>
2015-03-14 19:56:49 -0400

    updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ TARGET_PREBUILT_KERNEL := device/samsung/hlteskt/kernAl
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 #BOARD_CUSTOM_BOOTIMG_MK :=  device/samsung/hlteskt/custombootimg.mk
 
 # TWRP specific build flags
