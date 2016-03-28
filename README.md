179a4925337c52934cba0356cbf9d66ebbc54e3b
James Christopher Adduono <jc@adduono.com>
2016-03-28 03:27:06 -0400

    updates for update fixing issues
    Unified variants:
    lgt, ktt, skt
    (props set by init)
    
    Up to date f2fs support, fully working MTP & UMS
    Partial encryption support
    
    Change-Id: I961a3e6aa7b91d30597fc0a428ad12a1ebc81ba8

---
+++ README.md
@@ -0,0 +1,15 @@
+## TWRP device tree for Galaxy Note 3 (Korea)
+
+Add to `.repo/local_manifests/hlteskt.xml`:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+	<project path="device/samsung/hlteskt" name="android_device_samsung_hlteskt" remote="TeamWin" revision="android-6.0" />
+</manifest>
+```
+
+Then run `repo sync` to check it out.
+
+Kernel sources are available at: https://github.com/jcadduono/idleKernel-note3/tree/ik-twrp
+
--- omni_hlteskt.mk
+++ omni_hlteskt.mk
@@ -1,12 +1,13 @@
 # Release name
 PRODUCT_RELEASE_NAME := hlteskt
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/hlteskt/device.mk)
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hlteskt
 PRODUCT_NAME := omni_hlteskt
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := hlteskt
 PRODUCT_MANUFACTURER := samsung
